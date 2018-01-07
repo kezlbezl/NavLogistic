3e00e8546dd8953a6c9a207fd3661e91458ff667
Anton Kozlov(HOME) <kezlbezl@rambler.ru>
2018-01-07 22:05:31 +0200

    Changed tabSize: 4 to 2

--- server/models/Account.js
+++ server/models/Account.js
@@ -2,30 +2,30 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const AccountSchema = new Schema({
-    login: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    createdAt: {
-        type: Date
-    }
+  login: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  createdAt: {
+    type: Date
+  }
 });
 const Account = mongoose.model('Account', AccountSchema);
--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+  resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 })
 app.listen(7777, 'localhost', () => {
-    console.log('Server started on port 7777');
+  console.log('Server started on port 7777');
 })
