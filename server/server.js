7680c70e2ec1b4f2a4952802ae3880a376342ead
Anton Kozlov(HOME) <kezlbezl@rambler.ru>
2018-01-07 14:08:36 +0200

    Added test index.html

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.send('TEST RESPONSE');
+    resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 })
 app.listen(7777, 'localhost', () => {
     console.log('Server started on port 7777');
