2a122c03b7800ce21fdb0c02645b6fc7971ab555
Anton Kozlov(HOME) <kezlbezl@rambler.ru>
2018-01-07 22:46:22 +0200

    Some troubles with tabSize

--- server/db/db.js
+++ server/db/db.js
@@ -4,25 +4,25 @@ const model = require('../models/Account');
 const Account = mongoose.model('Account');
 
 module.exports = {
-  setUpConnection() {
-    mongoose.connect(`mongodb://localhost/navlogistic`)
-  },
-  getAccountList() {
-    return Account.find();
-  },
-  createAccount(data) {
-    const account = new Account({
-      login: data.login,
-      password: data.password,
-      email: data.email,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      description: data.description,
-      createdAt: new Date()
-    });
-    return account.save();
-  },
-  deleteAccount(id) {
-    return Account.findById(id).remove();
-  }
+    setUpConnection() {
+        mongoose.connect(`mongodb://localhost/navlogistic`)
+    },
+    getAccountList() {
+        return Account.find();
+    },
+    createAccount(data) {
+        const account = new Account({
+            login: data.login,
+            password: data.password,
+            email: data.email,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            description: data.description,
+            createdAt: new Date()
+        });
+        return account.save();
+    },
+    deleteAccount(id) {
+        return Account.findById(id).remove();
+    }
 }
--- server/server.js
+++ server/server.js
@@ -5,8 +5,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, resp) => {
-  resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+    resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 })
+app.get('/accounts', (req, resp) => {
+    resp.send();
+})
+app.post('/accounts', (req, resp) => {
+    resp.send();
+})
+app.delete('/accounts/:id', (req, resp) => {
+    resp.send();
+})
+
 app.listen(7777, 'localhost', () => {
-  console.log('Server started on port 7777');
+    console.log('Server started on port 7777');
 })
