7f6ba68acd79aaa66867251c967cfa9222734766
Anton Kozlov(HOME) <kezlbezl@rambler.ru>
2018-01-07 23:07:43 +0200

    Added config.json

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./db/db.js');
+
+db.setUpConnection();
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/', (req, resp) => {
     resp.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 })
 app.get('/accounts', (req, resp) => {
-    resp.send();
+    db.getAccountList()
+        .then(data => resp.send(data));
 })
 app.post('/accounts', (req, resp) => {
-    resp.send();
+    db.createAccount(req.body)
+        .then(data => resp.send(data));
 })
 app.delete('/accounts/:id', (req, resp) => {
-    resp.send();
+    db.deleteAccount(req.params.id)
+        .then(data => resp.send(data));
 })
 
 app.listen(7777, 'localhost', () => {
