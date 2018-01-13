d587aeddc592173f9369b8b7762ed19a483ccc2e
Anton Kozlov(HOME) <kezlbezl@rambler.ru>
2018-01-13 16:17:18 +0200

    Added Order model

---
+++ server/models/Order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const OrderSchema = new Schema({
+    company: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    }
+}, {
+    collection: 'OrderCollection'
+});
+const Order = mongoose.model('Order', OrderSchema);
